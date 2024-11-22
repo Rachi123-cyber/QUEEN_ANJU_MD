1f6d4be0bd8e0a5b7c23b94f24af7f6622c94c59
Rachi123-cyber <rachiyaartz33@gmail.com>
2024-11-22 21:10:57 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=CN8QHCSC#zDmiMXB-0QLx-AKCtmYLhwEh-evXpfE1zqp5ER8lBHs",
 };
